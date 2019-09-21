7c519841a8a296990be09e205860ce961428eabf
LuniakaAndrew <andrey.luniacka@yandex.ua>
2019-09-21 19:58:09 +0300

    Add Launcher.cs

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launcher : MonoBehaviour
+{
+    public static void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            Debug.LogError("Player Nickname is null or empty");
+            return;
+        }
+
+        //PhotonNetwork.NickName = value;
+    }
+}
